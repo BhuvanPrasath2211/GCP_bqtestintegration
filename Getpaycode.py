264db362abcf037e41dc52ba99259ff288556bd6
Drago <bhuvanprasathk@gmail.com>
2024-12-19 03:51:16 +0000

    Final working code

--- Getpaycode.py
+++ Getpaycode.py
@@ -30,7 +30,7 @@ def execute_query_with_dataframe(request):
                 query,
                 records_per_chunk,
                 file_prefix,
-                JSON_EXTRACT_SCALAR(date_summary, '$[0].DATE_RUN') AS date_run
+                JSON_EXTRACT_SCALAR(date_summary, '$.DATE_RUN') AS date_run
             FROM `st-npr-ukg-pro-data-hub-8100.UKG.delta`
             WHERE query_id = 2
             """
@@ -79,7 +79,7 @@ def execute_query_with_dataframe(request):
             SELECT 
             CAST(MAX(CAST(updateDtm AS DATETIME)) AS DATETIME) AS max_updateDtm_date
             FROM 
-            {query}
+            ({query})
             """
             
             query_job = bq_client.query(max_query)
@@ -90,6 +90,8 @@ def execute_query_with_dataframe(request):
                 
             if max_date is None:
                 raise ValueError("No max_updateDtm_date found.")
+            
+            max_date = max_date.strftime('%Y-%m-%dT%H:%M:%S.%f')
             
             new_date_summary = {
                 "DATE_RUN": max_date,
@@ -102,18 +104,16 @@ def execute_query_with_dataframe(request):
             update_query = """
             UPDATE `st-npr-ukg-pro-data-hub-8100.UKG.delta`
             SET date_summary = @new_date_summary
-            WHERE query_id = 7
+            WHERE query_id = 2
             """
 
-            query_parameters = [
-                bigquery.ScalarQueryParameter("new_date_summary", "STRING", new_date_summary_json)
-            ]
-
-            update_job = bq_client.query(
-                update_query,
-                job_config=bigquery.QueryJobConfig(query_parameters=query_parameters)
+            query_config = bigquery.QueryJobConfig(
+                query_parameters = [
+                 bigquery.ScalarQueryParameter("new_date_summary", "STRING", new_date_summary_json)
+                ]
             )
-            update_job.result()
+            
+            bq_client.query(update_query, job_config=query_config).result()
             logging.info("Updated date_summary column in the delta table.")
             
             
@@ -123,14 +123,22 @@ def execute_query_with_dataframe(request):
         #everything is now stored in var
         base_query, records_per_chunk, base_prefix, date_run = fetch_query_and_metadata(bq_client)
         
+        
         #Logic
         #Fullquery where the updateDtm is greater than the previous run
-        final_query=f"""
-        select * from {base_query}
-        where CAST(updateDtm AS DATETIME) > CAST({date_run} AS DATETIME)
-        """
+        final_query = f"""
+            SELECT * FROM ({base_query})
+            WHERE CAST(updateDtm AS DATETIME) > CAST(@date_run AS DATETIME)
+            """
+         
+        query_config = bigquery.QueryJobConfig(
+            query_parameters = [
+            bigquery.ScalarQueryParameter("date_run", "STRING", date_run)
+            ]
+        )  
+        
         #executing the query
-        query_job = bq_client.query(final_query)
+        query_job = bq_client.query(final_query,job_config=query_config)
         rows_iter = query_job.result(page_size=MAX_ROWS_PER_BATCH)
         accumulated_rows = []
         
@@ -150,31 +158,12 @@ def execute_query_with_dataframe(request):
         #finaly update the date for the next run
         update_date_run(bq_client,base_query)
         
-        
-        
-        
+        return jsonify({
+                "message": "CSV files created successfully",
+                "total_rows": total_rows,
+                "file_count": file_count
+            }), 200
 
-
-        
-        
-
-        
-            
-        
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        
-        
-        
         
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}", exc_info=True)
